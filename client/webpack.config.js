6e549d002f3c37ccadec5492139a4e100a333683
Colin Rent <colinrent@Colins-MacBook-Pro.local>
2024-03-07 00:48:40 -0500

    fixed webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,70 +1,67 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
-
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
+      editor: "./src/js/editor.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
-      new HtmlWebpackPlugin({
-        template: './src/index.html',
-        chunks: ['main'],
-        filename: 'index.html'
-      }),
-      new HtmlWebpackPlugin({
-        template: './src/install.html',
-        chunks: ['install'],
-        filename: 'install.html'
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
       }),
       new WebpackPwaManifest({
-        name: 'Budget Tracker',
-        short_name: 'Budget Tracker',
-        description: 'An application that allows you to track your budget.',
-        background_color: '#01579b',
-        theme_color: '#ffffff',
-        'theme-color': '#ffffff',
-        start_url: '/',
+        fingerprints: false,
+        inject: true,
+        name: "(Not) Just Any Text Editor",
+        short_name: "JATE",
+        description: "Description of your app",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        start_url: "./",
+        publicPath: "./",
         icons: [
           {
-            src: path.resolve('src/icons/icon-192x192.png'),
-            sizes: [192, 512],
-            destination: path.join('icons')
-          }
-        ]
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+        publicPath: "/",
       }),
-      new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'service-worker.js'
-      })
-      
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        filename: "index.html",
+      }),
     ],
 
     module: {
       rules: [
         {
-          test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
         },
         {
-          test: /\.m?js$/,
-          exclude: /(node_modules|bower_components)/,
+          test: /\.js$/,
+          exclude: /node_modules/,
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-transform-runtime']
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/plugin-transform-runtime",
+              ],
             },
           },
         },
